a918322aed451aadd21ad27af4f032979aa64acc
MarcAq11 <mcaquino0210val@student.fatima.edu.ph>
2026-09-16 14:15:26 +0800

    Week 3 Assignment #1 OOPR211

---
+++ OLFUVMV.java
@@ -0,0 +1,11 @@
+public class OLFUVMV {
+  public static void main(String[] args) {
+    System.out.println("Mission:\nThe Our Lady of Fatima University, together with Fatima Medical Science Foundation, Inc., is dedicated to the improvement of man as man through the holistic formation of individuals imbued with knowledge, skills, and virtues.\n");
+
+    System.out.println("Vision:\nA premier inclusive university of choice aspiring to improve man as man by developing individuals through a legacy of excellent education and compassionate value formation.\n");
+
+    System.out.println("Core Values:\nR = Respect\nI = Integrity\nS = Service\nE = Excellence\n");
+
+
+  }
+}
